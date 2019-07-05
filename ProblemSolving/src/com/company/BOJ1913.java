cb44a676ca32c652d34e3128a6c2981ba18216ef
Ilisha <40922963+Ilisha29@users.noreply.github.com>
2019-07-06 02:17:45 +0900

    spring lec summary

--- ProblemSolving/src/com/company/BOJ1913.java
+++ ProblemSolving/src/com/company/BOJ1913.java
@@ -1,4 +1,13 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class BOJ1913 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int FindNum = scanner.nextInt();
+
+        
+    }
 }

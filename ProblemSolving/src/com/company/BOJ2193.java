3ad271c32e5c3849b13e5c9b716408e6c8a2e8c6
Ilisha <40922963+Ilisha29@users.noreply.github.com>
2019-09-10 16:17:15 +0900

    BOJ2193

---
+++ ProblemSolving/src/com/company/BOJ2193.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class BOJ2193 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        if (N == 1) {
+            System.out.println(1);
+        }
+        if (N == 2) {
+            System.out.println(1);
+        }
+        if (N >= 3) {
+            long[] array = new long[N];
+            array[0] = 1;
+            array[1] = 1;
+            int i = 2;
+            while (i <= N - 1) {
+                array[i] = array[i - 1] + array[i - 2];
+                i++;
+            }
+            System.out.print(array[N - 1]);
+        }
+    }
+}

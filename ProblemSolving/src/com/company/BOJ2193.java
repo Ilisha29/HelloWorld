ccca524d4cd50e49f10b121df7e5a0a92d240968
Ilisha <40922963+Ilisha29@users.noreply.github.com>
2019-09-10 16:18:45 +0900

    feat :주의사항 주석 추가

--- ProblemSolving/src/com/company/BOJ2193.java
+++ ProblemSolving/src/com/company/BOJ2193.java
@@ -6,6 +6,7 @@ public class BOJ2193 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
+        //배열 접근에 주의!!!! N값이 1이거나 2일때 주의하기!
         if (N == 1) {
             System.out.println(1);
         }

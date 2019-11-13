8b79add699aeb0ef936780e3f41c4d509c6ffb89
Ilisha <40922963+Ilisha29@users.noreply.github.com>
2019-11-13 18:04:47 +0900

    PS

---
+++ ProblemSolving/src/com/company/Programmers주식가격.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Programmers주식가격 {
+    public static void main(String[] args) {
+        int[] prices = {1, 2, 3, 2, 3};
+        int[] answer = new int[prices.length];
+
+        for (int i = 0; i < prices.length ; i++) {
+            int notLow = 0;
+            for (int j = i+1; j < prices.length; j++) {
+                if(prices[i] <= prices[j]){
+                    notLow++;
+                }
+                if(prices[i] > prices[j] || j == prices.length-1){
+                    answer[i] = notLow;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < answer.length ; i++) {
+            System.out.println(answer[i]);
+        }
+
+        //answer 	[4, 3, 1, 1, 0] ;
+
+    }
+}

3090d72d758ca06811abc4aa976dd2fc1d1ed273
Ilisha <40922963+Ilisha29@users.noreply.github.com>
2020-01-01 21:53:44 +0900

    2019's

--- ProblemSolving/src/com/company/Ex.java
+++ ProblemSolving/src/com/company/Ex.java
@@ -1,4 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Ex {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] arr = new String[279];
+        for (int i = 0; i <  arr.length; i++) {
+            arr[i]= scanner.nextLine();
+        }
+
+        for (int i = 0; i < arr.length ; i++) {
+            System.out.println("'"+arr[i]+"',");
+        }
+    }
 }
--- ProblemSolving/src/com/company/Main.java
+++ ProblemSolving/src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+
+
     }
 }

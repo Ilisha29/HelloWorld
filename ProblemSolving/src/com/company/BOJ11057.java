b0871dee3dc0ff6df8dc9baf3db87a2f8f1f8480
Ilisha <40922963+Ilisha29@users.noreply.github.com>
2020-01-09 23:24:27 +0900

    BOJ #2

--- ProblemSolving/src/com/company/BOJ11057.java
+++ ProblemSolving/src/com/company/BOJ11057.java
@@ -1,4 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class BOJ11057 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int startNum = 0;
+        int maxNum = 1;
+        for (int i = 0; i < N; i++) {
+            maxNum *= 10;
+        }
+        while (startNum < maxNum) {
+
+        }
+        System.out.println(maxNum);
+    }
 }

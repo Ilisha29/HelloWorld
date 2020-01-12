0df15358c3aaf03afe109be103d1b1bc6266d5d9
Ilisha <40922963+Ilisha29@users.noreply.github.com>
2020-01-12 22:52:46 +0900

    BOJ1260 Start

---
+++ ProblemSolving/src/com/company/BOJ1260.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class BOJ1260 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}

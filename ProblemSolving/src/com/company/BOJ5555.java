3ace37cb270c8018f0aff5649ec956fec6da519d
Ilisha29 <94konzern@gmail.com>
2020-02-08 23:51:38 +0900

    add : BOJ PS

---
+++ ProblemSolving/src/com/company/BOJ5555.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class BOJ5555 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.next();
+        int tCase = scanner.nextInt();
+        String[] strings = new String[tCase];
+        for (int i = 0; i < tCase; i++) {
+            strings[i] = scanner.next();
+            strings[i] += strings[i];
+        }
+
+        int answer = 0;
+        for (int i = 0; i < strings.length ; i++) {
+            if(strings[i].contains(string)){
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
--- ProblemSolving/src/com/company/KAKAO2019_01.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-import java.util.Scanner;
-
-public class KAKAO2019_01 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-    }
-}

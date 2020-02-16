478718423fe635e555ddbe453bca922e00928dc6
Ilisha29 <94konzern@gmail.com>
2020-02-16 21:39:54 +0900

    add : BOJ PS

---
+++ ProblemSolving/src/com/company/BOJ2743.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class BOJ2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String string = bufferedReader.readLine();
+        System.out.println(string.length());
+        bufferedReader.close();
+    }
+}

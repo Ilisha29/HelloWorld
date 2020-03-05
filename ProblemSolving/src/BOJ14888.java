07283a6a0b7f32489341c2d6f6a8cc8e0602db8d
Ilisha29 <94konzern@gmail.com>
2020-03-05 23:22:22 +0900

    add : BOJ PS 14888 start

---
+++ ProblemSolving/src/BOJ14888.java
@@ -0,0 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ14888 {
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
---
+++ ProblemSolving/src/BOJ17144.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+// 18 : 10 start
+public class BOJ17144 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        bufferedReader.close();
+    }
+}

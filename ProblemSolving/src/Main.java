b783b2f7c67d563f2d05a08b8a42eec0f86df3ea
Ilisha29 <94konzern@gmail.com>
2020-03-08 00:56:20 +0900

    add : BOJ PS 5337 웰컴

--- ProblemSolving/src/Main.java
+++ ProblemSolving/src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        int[][] array = {{2, 3, 1}, {3, 1, 2}, {1, 2, 3}};
+        Arrays.sort(array);
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                System.out.print(array[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }

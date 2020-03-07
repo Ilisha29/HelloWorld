cae3f2b31849993e9039db7b0ee09744071f7539
Ilisha29 <94konzern@gmail.com>
2020-03-08 01:02:24 +0900

    add : BOJ PS 10926 jonas

---
+++ ProblemSolving/src/BOJ/BOJ10926.java
@@ -0,0 +1,11 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ10926 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        System.out.println(string+"??!");
+    }
+}

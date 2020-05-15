5634e318f49a763df223d2747ccfbf0366d1e45f
Ilisha29 <94konzern@gmail.com>
2020-05-16 00:38:38 +0900

    fix : 프로그래머스 해시 전화번호목록 end

--- ProblemSolving/src/Programmers/해시_빈출_상/전화번호목록.java
+++ ProblemSolving/src/Programmers/해시_빈출_상/전화번호목록.java
@@ -1,10 +1,9 @@
 package Programmers.해시_빈출_상;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 
-// 14 : 00
 public class 전화번호목록 {
     public static void main(String[] args) {
         String[][] strings = {{"119", "97674223", "1195524421"}, {"123", "456", "789"}, {"12", "123", "1235", "567", "88"}};
@@ -20,7 +19,18 @@ public class 전화번호목록 {
             phoneBooks.add(new PhoneBook(phone));
         }
         Collections.sort(phoneBooks);
-
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        hashMap.put(phoneBooks.get(0).numbers, 0);
+        for (int i = 1; i < phoneBooks.size(); i++) {
+            PhoneBook pb = phoneBooks.get(i);
+            for (String key : hashMap.keySet()) {
+                if (key.equals(pb.numbers.substring(0, key.length()))) {
+                    answer = false;
+                    break;
+                }
+            }
+            hashMap.put(pb.numbers, 0);
+        }
         return answer;
     }
 }

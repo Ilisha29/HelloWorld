d33393f95fc22168b9aae6859699294d2c77a74c
Ilisha29 <94konzern@gmail.com>
2021-03-17 22:29:22 +0900

    BOJ Greedy PS fail

---
+++ ProblemSolving/src/BOJ/Greedy/BOJ2437_.java
@@ -0,0 +1,25 @@
+package BOJ.Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ2437_ {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        int[] input = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(input);
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            if (sum + 1 < input[i]) {
+                break;
+            }
+            sum += input[i];
+        }
+        System.out.println(sum + 1);
+        bufferedReader.close();
+    }
+}
+

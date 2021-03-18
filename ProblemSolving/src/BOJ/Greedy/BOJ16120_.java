b87fd205d32726e7d508b065611798544b6efb88
Ilisha29 <94konzern@gmail.com>
2021-03-18 16:14:14 +0900

    BOJ Greedy PS fail

---
+++ ProblemSolving/src/BOJ/Greedy/BOJ16120_.java
@@ -0,0 +1,35 @@
+package BOJ.Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ16120_ {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = bufferedReader.readLine().split("");
+        System.out.println(printAnswer(input));
+        bufferedReader.close();
+    }
+
+    private static String printAnswer(String[] input) {
+        int count = 0;
+        for (int i = 0; i < input.length; i++) {
+            if (input[i].equals("P")) {
+                count++;
+                continue;
+            }
+            if (count >= 2 && i + 1 < input.length &&input[i + 1].equals("P")) {
+                count--;
+                i++;
+            } else {
+                return "NP";
+            }
+        }
+        if (count == 1) {
+            return "PPAP";
+        } else {
+            return "NP";
+        }
+    }
+}

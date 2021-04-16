1cdbcda4151c8887be6245ddb53b772de8c4ce8b
Ilisha29 <94konzern@gmail.com>
2021-04-16 14:35:39 +0900

    KAKAO PS 2019 winter intern No.01 && No.02

---
+++ ProblemSolving/src/KAKAO/KAKAO2019윈터인턴/KAKAO인턴_크레인2.java
@@ -0,0 +1,43 @@
+package KAKAO.KAKAO2019윈터인턴;
+
+import java.util.Stack;
+
+public class KAKAO인턴_크레인2 {
+    public static void main(String[] args) {
+        int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}};
+        int[] moves = {1, 5, 3, 5, 1, 2, 1, 4};
+        System.out.println(solution(board, moves));
+    }
+
+    public static int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer>[] stacks = new Stack[board.length];
+        for (int i = 0; i < stacks.length; i++) {
+            stacks[i] = new Stack<>();
+        }
+        for (int i = board.length - 1; i >= 0; i--) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[i][j] != 0) {
+                    stacks[j].add(board[i][j]);
+                }
+            }
+        }
+        Stack<Integer> dollStack = new Stack<>();
+        for (int i = 0; i < moves.length; i++) {
+            if (stacks[moves[i] - 1].size() != 0) {
+                int num = stacks[moves[i] - 1].pop();
+                if (dollStack.size() == 0) {
+                    dollStack.add(num);
+                } else {
+                    if (dollStack.peek() == num) {
+                        answer += 2;
+                        dollStack.pop();
+                    } else {
+                        dollStack.add(num);
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
---
+++ ProblemSolving/src/KAKAO/KAKAO2019윈터인턴/KAKAO인턴_튜플2.java
@@ -0,0 +1,44 @@
+package KAKAO.KAKAO2019윈터인턴;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class KAKAO인턴_튜플2 {
+    public static void main(String[] args) {
+        String[] strings = {"{{2},{2,1},{2,1,3},{2,1,3,4}}", "{{1,2,3},{2,1},{1,2,4,3},{2}}", "{{20,111},{111}}", "{{123}}", "{{4,2,3},{3},{2,3,4,1},{2,3}}"};
+        for (int i = 1; i < 2; i++) {
+            int[] result = solution(strings[i]);
+            for (int j = 0; j < result.length; j++) {
+                System.out.print(result[j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[] solution(String s) {
+        s = s.substring(2, s.length() - 2);
+        String[] splitString = s.split("},\\{");
+        Arrays.sort(splitString, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length() - o2.length();
+            }
+        });
+        int[] eachTupleSum = new int[splitString.length];
+        for (int i = 0; i < splitString.length; i++) {
+            int[] intTuple = Arrays.stream(splitString[i].split(",")).mapToInt(Integer::parseInt).toArray();
+            int sum = 0;
+            for (int j = 0; j < intTuple.length; j++) {
+                sum += intTuple[j];
+            }
+            eachTupleSum[i] = sum;
+        }
+        int num = 0;
+        int[] answer = new int[eachTupleSum.length];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = eachTupleSum[i] - num;
+            num = eachTupleSum[i];
+        }
+        return answer;
+    }
+}

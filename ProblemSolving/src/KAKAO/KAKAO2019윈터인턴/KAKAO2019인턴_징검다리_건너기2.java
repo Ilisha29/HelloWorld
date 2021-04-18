b072268a80703715f83cd5b7aa56418026c3d4c7
Ilisha29 <94konzern@gmail.com>
2021-04-18 18:46:42 +0900

    KAKAO 2019 INTERN BINARY SEARCH PS

---
+++ ProblemSolving/src/KAKAO/KAKAO2019윈터인턴/KAKAO2019인턴_징검다리_건너기2.java
@@ -0,0 +1,41 @@
+package KAKAO.KAKAO2019윈터인턴;
+
+public class KAKAO2019인턴_징검다리_건너기2 {
+    public static void main(String[] args) {
+        int[] stones = {2, 4, 5, 3, 2, 1, 4, 2, 5, 1};
+        int k = 3;
+        System.out.println(solution(stones, k));
+    }
+
+    public static int solution(int[] stones, int k) {
+        int start = 1;
+        int end = 200000000;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            int maxZeroLength = 0;
+            boolean isReadingZero = false;
+            int tmpMaxZeroLength = 0;
+            for (int i = 0; i < stones.length; i++) {
+                if (stones[i] <= mid) {
+                    isReadingZero = true;
+                    tmpMaxZeroLength++;
+                    if (i == stones.length - 1){
+                        maxZeroLength = Math.max(maxZeroLength, tmpMaxZeroLength);
+                    }
+                } else {
+                    if (isReadingZero) {
+                        maxZeroLength = Math.max(maxZeroLength, tmpMaxZeroLength);
+                        tmpMaxZeroLength = 0;
+                        isReadingZero = false;
+                    }
+                }
+            }
+            if (maxZeroLength < k) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
+}

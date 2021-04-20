c5d28b8545dc00144fb63787b152ebacfb76e830
Ilisha29 <94konzern@gmail.com>
2021-04-20 10:07:04 +0900

    BOJ Greedy PS

---
+++ ProblemSolving/src/BOJ/Sorting/BOJ18870.java
@@ -0,0 +1,31 @@
+package BOJ.Sorting;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ18870 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        int[] originNum = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] nums = originNum.clone();
+        Arrays.sort(nums);
+        Set<Integer> set = new LinkedHashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        int index = 0;
+        for (int num : set) {
+            map.put(num, index++);
+        }
+        StringBuilder stringBuilder = new StringBuilder("");
+        for (int i = 0; i < nums.length; i++) {
+            stringBuilder.append(map.get(originNum[i]) + " ");
+        }
+        System.out.print(stringBuilder);
+        bufferedReader.close();
+    }
+}

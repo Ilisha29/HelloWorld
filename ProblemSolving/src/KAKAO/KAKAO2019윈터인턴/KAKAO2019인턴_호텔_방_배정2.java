ac9ed7757275be0db41d59390ef277b74ff7138b
Ilisha29 <94konzern@gmail.com>
2021-04-27 15:29:47 +0900

    Programmers Union-Find PS

---
+++ ProblemSolving/src/KAKAO/KAKAO2019윈터인턴/KAKAO2019인턴_호텔_방_배정2.java
@@ -0,0 +1,43 @@
+package KAKAO.KAKAO2019윈터인턴;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class KAKAO2019인턴_호텔_방_배정2 {
+    public static void main(String[] args) {
+        long k = 10;
+        long[] room_num = {1, 3, 4, 1, 3, 1};
+        long[] answer = solution(k, room_num);
+        Arrays.stream(answer).forEach(System.out::println);
+    }
+
+    public static long[] solution(long k, long[] room_number) {
+        long[] answer = new long[room_number.length];
+        Map<Long, Long> reservationMap = new HashMap<>();
+        for (int i = 0; i < room_number.length; i++) {
+            long wishRoom = room_number[i];
+            if (reservationMap.containsKey(wishRoom)) {
+                long room = find(reservationMap, wishRoom);
+                answer[i] = room;
+                reservationMap.put(room, setting(reservationMap, room));
+            } else {
+                answer[i] = wishRoom;
+                reservationMap.put(wishRoom, wishRoom + 1);
+            }
+        }
+        return answer;
+    }
+
+    private static Long find(Map<Long, Long> reservationMap, long l) {
+        if (reservationMap.containsKey(l))
+            reservationMap.put(l, find(reservationMap, reservationMap.get(l)));
+        return reservationMap.containsKey(l) ? reservationMap.get(l) : l;
+    }
+
+    private static Long setting(Map<Long, Long> reservationMap, long l) {
+        if (reservationMap.containsKey(l))
+            reservationMap.put(l, find(reservationMap, reservationMap.get(l)));
+        return reservationMap.containsKey(l) ? reservationMap.get(l) : l + 1;
+    }
+}
--- ProblemSolving/src/ex.java
+++ ProblemSolving/src/ex.java
@@ -1,34 +1,5 @@
 public class ex {
-    private static int count = 0;
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5};
 
-        permutation(array, 0, 5, 5);
     }
-
-    private static void permutation(int[] array, int depth, int n, int k) {
-        if (depth == k) {
-            print2(array, k);
-        }
-        for (int i = depth; i < n; i++) {
-            swap2(array, i, depth);
-            permutation(array, depth + 1, n, k);
-            swap2(array, i, depth);
-        }
-    }
-
-    private static void swap2(int[] array, int i, int depth) {
-        int tmp = array[i];
-        array[i] = array[depth];
-        array[depth] = tmp;
-    }
-
-    private static void print2(int[] array, int k) {
-        for (int i = 0; i < k; i++) {
-            System.out.print(array[i] + " ");
-        }
-        count = count + 1;
-        System.out.println(count);
-    }
-
 }

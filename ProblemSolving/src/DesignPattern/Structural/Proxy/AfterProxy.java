0934d7e50769bef207b52bc2d5747d9eec6f3a96
Ilisha29 <94konzern@gmail.com>
2021-05-12 09:58:42 +0900

    Design Pattern : Proxy

--- ProblemSolving/src/DesignPattern/Structural/Proxy/AfterProxy.java
+++ ProblemSolving/src/DesignPattern/Structural/Proxy/AfterProxy.java
@@ -1,4 +1,33 @@
 package DesignPattern.Structural.Proxy;
 
 public class AfterProxy {
+    public static void main(String[] args) {
+        Subject proxy = new Proxy();
+        proxy.doAction();
+    }
+}
+
+
+interface Subject{
+    void doAction();
+}
+
+class RealSubject implements Subject{
+
+    @Override
+    public void doAction() {
+        System.out.println("핵심 로직 실행");
+    }
+}
+
+class Proxy implements Subject{
+    private RealSubject realSubject;
+
+    @Override
+    public void doAction() {
+        System.out.println("부가기능 1 ");
+        realSubject = new RealSubject();
+        realSubject.doAction();
+        System.out.println("부가기능 2");
+    }
 }
